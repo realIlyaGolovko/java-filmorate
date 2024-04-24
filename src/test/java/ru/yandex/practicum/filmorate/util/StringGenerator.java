18ae7ff7fbffc5e9de499554b2b232b97983b2cd
Ilya Golovko <i.i.golovko@tinkoff.ru>
2024-04-24 19:00:06 +0300

    test: add utility classes for tests

---
+++ src/test/java/ru/yandex/practicum/filmorate/util/StringGenerator.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.util;
+
+import java.util.Random;
+
+public class StringGenerator {
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return sb.toString();
+    }
+}
