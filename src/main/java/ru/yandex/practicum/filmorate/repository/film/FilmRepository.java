aae8e1cee864d713f50bb4239b441f5e8310d749
Ilya Golovko <i.i.golovko@tinkoff.ru>
2024-05-10 11:40:02 +0300

    refactor: upgrade Film logic

--- src/main/java/ru/yandex/practicum/filmorate/repository/film/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/film/FilmRepository.java
@@ -1,8 +1,9 @@
-package ru.yandex.practicum.filmorate.repository;
+package ru.yandex.practicum.filmorate.repository.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FilmRepository {
     Film saveFilm(final Film film);
@@ -10,4 +11,6 @@ public interface FilmRepository {
     Film updateFilm(final Film film);
 
     List<Film> getFilms();
+
+    Optional<Film> getFilm(final Long filmId);
 }
