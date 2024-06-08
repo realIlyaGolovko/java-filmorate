5d4e8a05e7c219154a9f65cc6492e60bf8658166
Ilya Golovko <i.i.golovko@tinkoff.ru>
2024-06-08 10:39:05 +0300

    feature: add mappers

---
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FilmRowMapper.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.film.Film;
+import ru.yandex.practicum.filmorate.model.film.Mpa;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class FilmRowMapper implements RowMapper<Film> {
+    @Override
+    public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return Film.builder()
+                .id(rs.getLong("film_id"))
+                .name(rs.getString("name"))
+                .description(rs.getString("description"))
+                .releaseDate(rs.getDate("release_date").toLocalDate())
+                .duration(rs.getInt("duration"))
+                .mpa(new Mpa(rs.getLong("mpa_rating_id"), rs.getString("mpa_name")))
+                .build();
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/GenreRowMapper.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.film.Genre;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class GenreRowMapper implements RowMapper<Genre> {
+    @Override
+    public Genre mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Genre(rs.getLong("genre_id"), rs.getString("name"));
+    }
+}
